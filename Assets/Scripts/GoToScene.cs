81a39456738bb027a0b57aafc5ad057cff5ba3f0
Zock <zock868@gmail.com>
2025-08-07 21:42:06 +0200

    jimpu 0.5.5
    Some new multiplayer things

---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,20 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoToScene : MonoBehaviour
+{
+    public string SceneToGoTo;
+    public Button Button;
+    void Start()
+    {
+        Button.onClick.AddListener(ButtonPresed);
+
+    }
+    void ButtonPresed()
+    {
+        Debug.Log("Loaded scene: " + SceneToGoTo);
+        SceneManager.LoadScene(SceneToGoTo);
+    }
+}
