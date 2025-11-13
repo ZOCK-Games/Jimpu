28707844c7ecbe0e00804a6eacd75cca174d228f
Zock <zock868@gmail.com>
2025-11-13 19:11:26 +0100

    jimpu 0.5.19
    Added the function for voice to shoot and a simple speech bubble

--- Assets/Scripts/BulletShootVoice.cs
+++ Assets/Scripts/BulletShootVoice.cs
@@ -1,40 +1,45 @@
 using System.Collections;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class BulletShootVoice : MonoBehaviour
 {
-    public GameObject Bullet;
+    public List<GameObject> Bullets;
     public Transform BulletContainer;
     void Start()
     {
-        StartCoroutine(Shoot(new Vector3(1000, 0, 0), Bullet, new Vector3(0, 0, 0), 10f));
     }
 
     // Update is called once per frame
-    IEnumerator Shoot(Vector3 Target, GameObject Bullet, Vector3 ShotingPosition, float BulletSpeed)
+    public IEnumerator Shoot(Transform Target, GameObject Bullet, Transform ShotingPosition, float BulletSpeed, int BulletCount, float BulletShotCountdown)
     {
-        yield return new WaitForSeconds(4f);
-        ///
-        /// This Part moves the Bullet and rotates it
-        float duration = Vector3.Distance(ShotingPosition, Target) / BulletSpeed;
-        float elapsedTime = 0f;
-        Debug.Log($"Duration: {duration}");
-        GameObject BulletObj = Instantiate(Bullet);
-        BulletObj.transform.SetParent(BulletContainer);
-        BulletObj.transform.position = ShotingPosition;
-        while (elapsedTime < duration && Bullet)
+        Debug.Log($"Summoning {BulletCount} Bullets");
+        for (int i = 0; i < BulletCount; i++)
         {
-            BulletObj.transform.Rotate(Vector3.up, (elapsedTime * 30) * Time.deltaTime, Space.World);
-            BulletObj.transform.Rotate(Vector3.left, (elapsedTime * 30) * Time.deltaTime, Space.World);
-            BulletObj.transform.position = Vector3.Lerp(ShotingPosition, Target, (elapsedTime / duration));
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-        if (Bullet)
-        {
-            BulletObj.transform.position = Target;
-            Destroy(BulletObj, 20f);
+            Vector3 TargetPos = Target.position;
+            Vector3 ShotingPos = ShotingPosition.position;
+
+            float duration = Vector3.Distance(ShotingPos, TargetPos) / BulletSpeed;
+            float elapsedTime = 0f;
+            Debug.Log($"Duration: {duration}");
+            GameObject BulletObj = Instantiate(Bullet);
+            BulletObj.transform.SetParent(BulletContainer);
+            BulletObj.transform.position = ShotingPos;
+            while (elapsedTime < duration && BulletObj != null)
+            {
+                BulletObj.transform.Rotate(Vector3.up, (elapsedTime * 30) * Time.deltaTime, Space.World);
+                BulletObj.transform.Rotate(Vector3.left, (elapsedTime * 30) * Time.deltaTime, Space.World);
+                BulletObj.transform.position = Vector3.Lerp(ShotingPos, TargetPos, (elapsedTime / duration));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            if (BulletObj != null)
+            {
+                BulletObj.transform.position = TargetPos;
+                Destroy(BulletObj, 20f);
+            }
+            yield return new WaitForSeconds(BulletShotCountdown);
         }
     }
 
--- Assets/VoiceManager.cs
+++ Assets/VoiceManager.cs
@@ -9,9 +9,17 @@ public class VoiceManager : MonoBehaviour
     [SerializeField] private Animator VoiceAnimator;
     [SerializeField] private TextMeshProUGUI TextField;
     [SerializeField] private PlayerControll playerControll;
+    [SerializeField] private BulletShootVoice bulletShootVoice;
+    [Header("AttackSettings")]
+    public Transform ShootingPosition;
+    public float MaxBulletSpeed;
+    public float MinBulletSpeed;
+    public float MaxBulletShotCountdown;
+    public float MinBulletShotCountdown;
 
     void Start()
     {
+        StartCoroutine(WalkLeftWaiting());
     }
     IEnumerator WalkLeftWaiting()
     {
@@ -21,17 +29,35 @@ public class VoiceManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         yield return new WaitUntil(() => Input.anyKey);
         yield return new WaitForSeconds(0.5f);
+        Mode -= 1;
         TextField.text = "If you haven't heard me i SAID PLEAS MOVE LEFT NOW!";
+        yield return new WaitForSeconds(1f);
+        yield return new WaitUntil(() => Input.anyKey);
+        yield return new WaitForSeconds(0.5f);
+        TextField.text = "Thats enough You wont make it out alive!";
+
+        StartCoroutine(bulletShootVoice.Shoot(
+        playerControll.Player.transform,
+        bulletShootVoice.Bullets[0],
+        ShootingPosition,
+        Random.Range(MinBulletSpeed,
+        MaxBulletSpeed),
+        10,
+        Random.Range(MaxBulletShotCountdown,MinBulletShotCountdown)));
+
+        playerControll.rb.linearVelocityX = 0.31f;
+
     }
     void Update()
     {
-        if (playerControll.rb.linearVelocityX > 0.3f)
+        if (playerControll.rb.linearVelocityX > 0.6f)
         {
             StopCoroutine(WalkLeftWaiting());
             TextField.text = "I knew you can do it (=";
             VoiceAnimator.SetBool("Hello", false);
+            VoiceAnimator.SetTrigger("AllOut");
         }
-    }
 
+    }
 
 }
